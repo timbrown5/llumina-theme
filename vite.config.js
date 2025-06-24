3db533a5597b71e5815e927a10d4d0a0fc8fca17
Tim Brown <bittebrown@gmail.com>
2025-06-24 09:02:37 +0100

    Try using different base path in prod

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/lumina-theme/' : './',
   server: {
     host: '0.0.0.0',
     port: 3000,
